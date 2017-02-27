19387ac7f306b8a8f99ccca1732108f050c0e5f9
jomno <jake8955@naver.com>
2017-02-27 19:55:53 +0900

    블로그 crud 하는 중

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'boards/header'
   get 'boards/common_post_new'
 
+  resources :blog
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226064139) do
+ActiveRecord::Schema.define(version: 20170227094203) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "blogs", ["user_id"], name: "index_blogs_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
