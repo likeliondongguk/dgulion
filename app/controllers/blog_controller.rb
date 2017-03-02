d61f93e54a613f2ccab9372e58ded14d6d5b08ec
jomno <jake8955@naver.com>
2017-03-02 20:01:44 +0900

    post-tagging-tag 로직 구현 (메소드 구현 전)

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,39 +1,53 @@
 class BlogController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy ]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
-     @blogs=Blog.all
-    # @blogs=Tag.find_by(name: "blog").posts
+    #@blogs=Blog.all
+    @blogs=Tag.find_by_name("blog").posts
   end
+
   def show
 
   end
+
   def new
-     @blog = Blog.new
-    #@blog = Post.new
+    #@blog = Blog.new
+    @blog = Post.new
   end
+
   def create
-    @blog=Blog.new(blog_params)
-    # @blog=Post.new(blog_params)
+    # @blog=Blog.new(blog_params)
+    @blog=Post.new(blog_params)
     @blog.save
-    redirect_to @blog
+    Tagging.create(post_id: @blog.id, tag_id: 1)
+    redirect_to set_url(:show, @blog.id)
   end
+
   def edit
   end
+
   def update
     @blog.update(blog_params)
-    redirect_to @blog
+    redirect_to set_url(:show, @blog.id)
   end
+
   def destroy
     @blog.destroy
-    redirect_to @blog
+    redirect_to url_for(controller: :blog, action: :index)
   end
 
   private
   def set_blog
-    @blog=Blog.find(params[:id])
+    #@blog=Blog.find(params[:id])
+    @blog=Tag.find_by_name("blog").posts.find(params[:id])
   end
+
+  def set_url(action, id)
+    url_for(controller: :blog, action: action, id: id)
+  end
+
   def blog_params
-    params.require(:blog).permit(:title, :content, :user_id)
+    params.require(:post).permit(:title, :content, :user_id)
   end
 
 end
