754c28672529d40520bca934c7ff7e389e159041
Sungjun <tjdwns9498@likelion.org>
2017-03-11 05:06:02 +0900

    Merge branch 'nojong' of https://github.com/likeliondongguk/dgulion into sungjun
    # Conflicts:
    #	db/schema.rb
    #	public/assets/images/demo/.DS_Store

---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,75 @@
+class SubmissionsController < ApplicationController
+  before_action :set_submission, only: [:show, :edit, :update, :destroy]
+
+  # GET /submissions
+  # GET /submissions.json
+  def index
+      #how to show submisstions that are belonged to task/1 task/2 task/3 ...
+    if params[:search]
+      @submissions = Submission.search(params[:search]).order("created_at DESC")
+    else
+      @submissions = Submission.all.order('created_at DESC')
+    end
+
+    @paginator, @submissions = paginate :submissions, :per_page => 5,
+                                        :conditions => ['name Like ?', "%#{params[:search]}%"],
+                                        :order => 'name'
+  end
+
+  # GET /submissions/1
+  # GET /submissions/1.json
+  def show
+  end
+
+  # GET /submissions/new
+  def new
+    @submission = Submission.new
+  end
+
+  # GET /submissions/1/edit
+  def edit
+  end
+
+  # POST /submissions
+  # POST /submissions.json
+  def create
+    @submission=Submission.new(submission_params)
+    @submission.save
+    redirect_to url_for(controller: :submissions, action: :show, id: @submission.id)
+  end
+
+  # PATCH/PUT /submissions/1
+  # PATCH/PUT /submissions/1.json
+  def update
+    respond_to do |format|
+      if @submission.update(submission_params)
+        format.html { redirect_to @submission, notice: 'Submission was successfully updated.' }
+        format.json { render :show, status: :ok, location: @submission }
+      else
+        format.html { render :edit }
+        format.json { render json: @submission.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /submissions/1
+  # DELETE /submissions/1.json
+  def destroy
+    @submission.destroy
+    respond_to do |format|
+      format.html { redirect_to submissions_url, notice: 'Submission was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_submission
+      @submission = Submission.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def submission_params
+      params.require(:submission).permit(:title, :content)
+    end
+end
