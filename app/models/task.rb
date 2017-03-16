381e3a32e4e48fe0a73fe093fcc5ab903a8dacf0
Sungjun <tjdwns9498@likelion.org>
2017-03-17 04:35:45 +0900

    1+3probs

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,10 +3,10 @@ class Task < ActiveRecord::Base
   has_many :users, through: :t_rates
 
 
-  def is_writer?(u)
-    return true if !u.nil? && u == self.user
-    false
+  def is_completed?(c)
+    return true if !c.nil && c == self.user
   end
+
 
   def self.search(search)
     where("content || title LIKE ?", "%#{search}%")
